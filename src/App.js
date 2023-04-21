490836eec785400ae3922bf4b2d92f64bfacb649
Diego Jiménez <Diego@Tanias-MacBook-Pro.local>
2023-04-20 19:24:02 -0600

    adding logic and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import './hojas-de-estilo/Boton.css';
+import './hojas-de-estilo/Contador.css';
+import Contador from './componentes/Contador';
+import Boton from './componentes/Boton';
+import freeCodeCampLogo from './imagenes/fcc_primary_large.jpeg';
+import {useState} from 'react';
 
 function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    console.log('(+) Click');
+    setNumClics(numClics + 1);
+  }
+
+  const restarClic = () => {
+    console.log('(-) Click');
+    setNumClics(numClics - 1);
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+    setNumClics(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className ='frecodecamp-logo-contenedor'>
+        <img 
+        className ='freecodecamp-logo'
+        src = {freeCodeCampLogo}
+        alt ='Logo de Feecodecamp'/>
+      </div>
+      <div className ='contenedor-principal'>
+        <Contador numClics = {numClics} />
+        <Boton 
+          texto ='(+) Click'
+          esBotonDeClic = {true}
+          manejarClic = {manejarClic} />
+        <Boton 
+          texto =' (-) Click'
+          esBotonDeClic = {true}
+          manejarClic = {restarClic} />
+        <Boton
+          texto = 'Reiniciar'
+          esBotonDeClic = {false}
+          manejarClic = {reiniciarContador} />
+      </div>
     </div>
   );
 }
